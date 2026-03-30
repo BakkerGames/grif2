bcc5dd8aa3bdb95c4c50827f4bc0b31a4c710ab4
Scott Bakker <sbakker343@gmail.com>
2026-03-30 15:58:00 -0400

    v2026.03.30

--- griflib/Common.cs
+++ griflib/Common.cs
@@ -5,7 +5,7 @@ public static partial class Common
     /// <summary>
     /// Gets the current version of the library.
     /// </summary>
-    public static string Version { get { return "2.2026.3.19"; } }
+    public static string Version { get { return "2.2026.3.30"; } }
 
     /// <summary>
     /// String comparison: OrdinalIgnoreCase
